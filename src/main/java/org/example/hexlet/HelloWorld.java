9adf0ca555ccfb2ee067f8811262fac509c0f765
Igor <82278294+Siletskiy-igor@users.noreply.github.com>
2023-08-30 19:31:51 +0300

    make new route

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,11 +10,21 @@ public class HelloWorld {
         });
         // Описываем что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
-        app.get("/users", ctx -> ctx.result("GET /users"));
-        app.post("/users", ctx -> ctx.result("POST /users"));
+//        app.get("/users", ctx -> ctx.result("GET /users"));
+//        app.post("/users", ctx -> ctx.result("POST /users"));
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name);
+        });
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParam("id");
+            ctx.result("Course id is: " + id);
+        });
+
+        app.get("/users/{user-id}/posts/{post-id}", ctx -> {
+            var userId = ctx.pathParam("user-id");
+            var postId = ctx.pathParam("post-id");
+            ctx.result("User ID: " + userId + " post id: " + postId);
         });
         app.start(7070); // Стартуем веб-сервер
     }
